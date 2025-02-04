f340fd598b3a7f8a846e0573d086f08ae2037e34
rezaprasetya241 <rezaprasetya241@gmail.com>
2025-02-04 21:06:53 +0700

    fix package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Dropdown from "./components/dropdown/dropdown";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Dropdown />
+      <Dropdown
+        label={"label"}
+        placeholder={""}
+        isMultiple={false}
+        withSearch={false}
+        options={[]}
+        outlined={false}
+      />
     </>
   );
 }
